538439875a8190f65ea1ef86c93ba9b59ad8ae8a
saitohiroto <saito@lclco.com>
2018-03-16 12:29:01 +0900

    intern_trainning3 ドメイン（キー）毎に出力

---
+++ training3.rb
@@ -0,0 +1,24 @@
+# アスタリスク引数は設定しておけば、配列になるのでメソッドを呼び出すと、いくらでも変数を入れられる。
+def express_addresses(*address)
+
+  addresses = address.group_by{ |domain| domain }
+
+  puts addresses
+
+   addresses.each do |address|
+     key = address[0]
+     number = address[1].size
+     puts "#{key} "
+     puts number
+   end
+end
+
+express_addresses(
+    "asia@gmail.com", "india@gmail.com" , "asia@ezweb.ne.jp" ,"india@gmail.com",
+    "asia@gmail.com" , "india@gmail.com"
+)
+
+# うまくハッシュに変換できた。
+# array = [[:japan,"yen"],[:india,"rupee"],[:us,"dollar"]]
+# h = array.to_h
+# puts h.class
