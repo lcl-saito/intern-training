f7b4bb431107335d7a44f36df46ee4a301a79bd6
saitohiroto <saito@lclco.com>
2018-03-20 11:13:23 +0900

    intern_trainning3　仮完成

--- training3.rb
+++ training3.rb
@@ -1,48 +1,17 @@
-def express_addresses(addresses)
+def express_addresses(address)
 
-  addresses.each do |address|
-    element = address.split("@")
-    elements = Hash[*element]
-    puts elements
+  addresses = address.group_by {|domain| domain.split("@")[1]}
 
-  end
-
+ addresses.each do |address|
+   key = address[0]
+   number = address[1].size
+   puts "#{key}: #{number}"
+   end
 end
 
-addresses = [
-    "asia@gmail.com", "japan@gmail.com" , "asia@yahoo.jp" , "india@ezweb.ne.jp" ,
+address = [
+    "asia@gmail.com", "japan@gmail.com" , "asia@yahoo.jp" ,   "india@ezweb.ne.jp" , "korea@yahoo.jp" ,
+    "china@gmail.com" , "mongol@softbank.ne.jp" ,
 ]
 
-express_addresses(addresses)
-
-
-
-# asia = "asia@gmail.com".split("@")
-# puts asia[1]
-
-# 空のハッシュを作る
-#   domains = {}
-# number = 1
-#    addresses.each do |address|
-#      if address.include?("ドメイン")
-#        domains{"ドメイン"} => number
-#        number += 1
-#      end
-#
-#    end
-
-#  addresses.each do |address|
-#     if address.include?("gmail.com")
-#       gmail += 1
-#     elsif address.include?("yahoo.jp")
-#       yahoo += 1
-#     elsif  address.include?("ezweb.ne.jp")
-#       ezweb += 1
-#     end
-#   end
-#
-# domains = {"gmail.com" => gmail , "yahoo.jp" => yahoo , "ezweb.ne.jp" => ezweb}
-#
-#   domains.each do |domain|
-#     puts "#{domain[0]} : #{domain[1]}"
-#   end
+express_addresses(address)
